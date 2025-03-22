0a83c718ce0aa6478ee904d8227d9fb3eda5aa0c
Masum Billah Zihadi <zihadimasumbillah@gmail.com>
2025-03-23 05:12:55 +0600

    initial commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,23 +5,14 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: 'postgres',
+  dialectModule: require('pg'),
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
   },
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  define: {
-    timestamps: true,
-    paranoid: true
-  }
+  logging: false
 });
 
 export default sequelize;
