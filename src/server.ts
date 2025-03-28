a89b26cfdd6558469d9d770ba8e4c59baa27e393
Masum Billah Zihadi <zihadimasumbillah@gmail.com>
2025-03-28 13:29:47 +0600

    lateastpush

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,7 @@ const allowedOrigins = [
   'http://127.0.0.1:3000', 
   'http://localhost:3001',
   'https://useradminstration-tjm1-fi9jaxiyl-masum-billah-s-projects.vercel.app', 
+  'https://useradminstration-tjm1.vercel.app',
   'https://useradminstration-itransition.vercel.app'
 ];
 
