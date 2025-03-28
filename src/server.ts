e415c072e7b72ca135c81c328543ef6716869b06
Masum Billah Zihadi <zihadimasumbillah@gmail.com>
2025-03-28 13:28:42 +0600

    lateastpush

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,9 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000', 
   'http://127.0.0.1:3000', 
-  'http://localhost:3001'
+  'http://localhost:3001',
+  'https://useradminstration-tjm1-fi9jaxiyl-masum-billah-s-projects.vercel.app', 
+  'https://useradminstration-itransition.vercel.app'
 ];
 
 if (process.env.VERCEL_URL) {
